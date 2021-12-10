73f6840df9b0a133e98b76f9e0901de7837e9f28
Ola NguyenVan <olanguyenvan@gmail.com>
2021-12-10 12:00:35 +0100

    feat(Day10): Add solution for part 2

--- src/days/day10/index.ts
+++ src/days/day10/index.ts
@@ -1,5 +1,5 @@
 import * as path from "https://deno.land/std/path/mod.ts";
-import { getSyntaxErrorScore } from "./parentheses.ts";
+import { getSyntaxErrorScore, getAutoCompleteScore } from "./parentheses.ts";
 
 const __dirname = path.dirname(path.fromFileUrl(import.meta.url));
 const pathToInput = path.resolve(__dirname, `input`);
@@ -12,6 +12,7 @@ function main(rawInput: string) {
 
     solvePart1(parenthesesLines);
     console.log("\n");
+    solvePart2(parenthesesLines);
 }
 
 function parseParenthesesLines(rawInput: string): string[][] {
@@ -26,12 +27,22 @@ function parseParenthesesLines(rawInput: string): string[][] {
 }
 // =======================
 
-function solvePart1(parseParenthesesLines: string[][]): void {
+function solvePart1(parentheses: string[][]): void {
     const t0 = performance.now();
 
-    const syntaxErrorScore = getSyntaxErrorScore(parseParenthesesLines);
+    const syntaxErrorScore = getSyntaxErrorScore(parentheses);
     const t1 = performance.now();
 
     console.log(`Answer to part 1 is: ${syntaxErrorScore}.`);
     console.log(`Solution to part 1 was solved in ${t1 - t0} milliseconds.`);
+}
+
+function solvePart2(parentheses: string[][]): void {
+    const t0 = performance.now();
+
+    const autoCompleteScore = getAutoCompleteScore(parentheses);
+    const t1 = performance.now();
+
+    console.log(`Answer to part 2 is: ${autoCompleteScore}.`);
+    console.log(`Solution to part 2 was solved in ${t1 - t0} milliseconds.`);
 }
--- src/days/day10/parentheses.ts
+++ src/days/day10/parentheses.ts
@@ -1,11 +1,17 @@
 const OPENING_PARENTHESES = ["{", "", "(", "<", "["];
-const CLOSING_PARENTHESES = ["}", ")", ">", "]"];
 
 const PARENTHESES_ERROR_SCORE: { [key: string]: number } = {
     ")": 3,
     "]": 57,
     "}": 1197,
     ">": 25137,
+};
+
+const AUTO_COMPLETE_SCORE: { [key: string]: number } = {
+    ")": 1,
+    "]": 2,
+    "}": 3,
+    ">": 4,
 };
 
 const CLOSE_PARENTHESES: { [key: string]: string } = {
@@ -39,4 +45,69 @@ export function getSyntaxErrorScore(parentheses: string[][]): number {
     }
 
     return score;
+}
+
+function getUncorruptedParentheses(parentheses: string[][]): string[][] {
+    return parentheses.filter((parenthesesLine) => {
+        const stack: string[] = [];
+
+        for (let i = 0; i < parenthesesLine.length; i++) {
+            const currentParenthes = parenthesesLine[i];
+
+            if (OPENING_PARENTHESES.includes(currentParenthes)) {
+                stack.push(currentParenthes);
+            } else {
+                const lastParenthes = stack.pop()!;
+                if (currentParenthes !== CLOSE_PARENTHESES[lastParenthes]) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    });
+}
+
+function getAutoComplete(parentheses: string[]): string[] {
+    const stack: string[] = [];
+
+    for (let i = 0; i < parentheses.length; i++) {
+        const currentParenthes = parentheses[i];
+
+        if (OPENING_PARENTHESES.includes(currentParenthes)) {
+            stack.push(currentParenthes);
+        } else {
+            const lastParenthes = stack.pop()!;
+        }
+    }
+
+    let autoComplete = [];
+
+    for (let i = stack.length - 1; i >= 0; i--) {
+        autoComplete.push(CLOSE_PARENTHESES[stack[i]]);
+    }
+
+    return autoComplete;
+}
+
+export function getAutoCompleteScore(parentheses: string[][]): number {
+    const scores: number[] = [];
+    const notCorruptedParentheses = getUncorruptedParentheses(parentheses);
+
+    for (const parenthesesLine of notCorruptedParentheses) {
+        const autoComplete = getAutoComplete(parenthesesLine);
+
+        const currentScore = autoComplete.reduce(
+            (acc: number, parenthes: string) => {
+                return acc * 5 + AUTO_COMPLETE_SCORE[parenthes];
+            },
+            0
+        );
+
+        scores.push(currentScore);
+    }
+
+    scores.sort((a: number, b: number) => a - b);
+
+    return scores[Math.floor(scores.length / 2)];
 }
