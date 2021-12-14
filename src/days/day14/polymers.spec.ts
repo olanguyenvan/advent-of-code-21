29faf748b0d307db9df7b89f82c3f74ecd3b63d3
Ola Nguyen Van <olanguyenvan@gmail.com>
2021-12-14 18:17:55 +0100

    feat(Day14): Add  solution with caching for part 2
    * feat(Day14): Add not full solution for part 2
    
    * remove comments
    
    * improved solution for part2: works for 20 steps
    
    * almost working solution. steps=7 has difference in count for B
    
    * Working solution for part 2
    
    * clean up

---
+++ src/days/day14/polymers.spec.ts
@@ -0,0 +1,223 @@
+import { assertEquals } from "https://deno.land/std@0.117.0/testing/asserts.ts";
+import {
+    describe,
+    each,
+    it,
+} from "https://deno.land/x/test_suite@0.9.1/mod.ts";
+import {
+    elementsCount,
+    polymer,
+    insertions,
+    getPolymerElementsCountAfterInsertions,
+    getOverlappingSubPolymers,
+    getPolymerAfterInsertions,
+} from "./polymers.ts";
+
+describe("getPolymerAfterInsertions", () => {
+    each(
+        "ola with only 'a' after %s steps should be %s",
+        [
+            [0, "ola"],
+            [1, "oalaa"],
+            [2, "oaaalaaaa"],
+            [3, "oaaaaaaalaaaaaaaa"],
+        ],
+
+        (steps: number, expectedPolymer: string) => {
+            const initialPolymer = "ola";
+            const insertions: insertions = {
+                ol: "a",
+                oa: "a",
+                al: "a",
+                la: "a",
+                aa: "a",
+            };
+
+            const resultPolymer = getPolymerAfterInsertions(
+                initialPolymer,
+                insertions,
+                steps
+            );
+
+            assertEquals(resultPolymer, expectedPolymer);
+        }
+    );
+
+    each(
+        "string of character 1 does not get longer",
+        [[0], [1], [2]],
+
+        (steps: number) => {
+            const initialPolymer = "o";
+            const insertions: insertions = {
+                ol: "a",
+                oa: "a",
+            };
+
+            const resultPolymer = getPolymerAfterInsertions(
+                initialPolymer,
+                insertions,
+                steps
+            );
+
+            assertEquals(resultPolymer, initialPolymer);
+        }
+    );
+});
+
+describe("getOverlappingSubPolymers", () => {
+    it("divides when polymer length is divisible by inteval", () => {
+        const resultDivision = getOverlappingSubPolymers("olalandoug", 4);
+        const expectedDivisions = ["olal", "land", "doug"];
+
+        assertEquals(resultDivision, expectedDivisions);
+    });
+
+    it("divides when polymer length is not divisible by inteval", () => {
+        const resultDivision = getOverlappingSubPolymers("olalandou", 4);
+        const expectedDivisions = ["olal", "land", "dou"];
+
+        assertEquals(resultDivision, expectedDivisions);
+    });
+
+    it("does not divide when it's already meeting requirements", () => {
+        const resultDivision = getOverlappingSubPolymers("olal", 4);
+        const expectedDivisions = ["olal"];
+
+        assertEquals(resultDivision, expectedDivisions);
+    });
+
+    it("divides into 2", () => {
+        const resultDivision = getOverlappingSubPolymers("123456789", 5);
+        const expectedDivisions = ["12345", "56789"];
+
+        assertEquals(resultDivision, expectedDivisions);
+    });
+});
+
+describe("getPolymerElementsCountAfterInsertions", () => {
+    each(
+        "ola with only 'a' after %s steps should be %s",
+        [
+            [0, { o: 1, l: 1, a: 1 }],
+            [1, { o: 1, l: 1, a: 3 }],
+            [2, { o: 1, l: 1, a: 7 }],
+            [3, { o: 1, l: 1, a: 15 }],
+            [4, { o: 1, l: 1, a: 31 }],
+        ],
+
+        (steps: number, expectedElementsCount: elementsCount) => {
+            const initialPolymer = "ola";
+            const insertions: insertions = {
+                ol: "a",
+                oa: "a",
+                al: "a",
+                la: "a",
+                aa: "a",
+            };
+
+            const resultPolymer = getPolymerElementsCountAfterInsertions(
+                initialPolymer,
+                insertions,
+                steps
+            );
+
+            assertEquals(resultPolymer, expectedElementsCount);
+        }
+    );
+
+    it("olaladou => 0 steps => o2l2a2d1u1", () => {
+        const initialPolymer = "olaladou";
+
+        const insertions = {
+            ol: "a",
+            la: "a",
+            al: "a",
+            ad: "a",
+            do: "a",
+            ou: "a",
+        };
+
+        // oalaaalaaadaoau
+        const expectedElementsCount = {
+            o: 2,
+            d: 1,
+            a: 2,
+            l: 2,
+            u: 1,
+        };
+
+        const resultPolymer = getPolymerElementsCountAfterInsertions(
+            initialPolymer,
+            insertions,
+            0
+        );
+
+        assertEquals(resultPolymer, expectedElementsCount);
+    });
+
+    it("olaladou => o2d1a9l2u1", () => {
+        const initialPolymer = "olaladou";
+
+        const insertions = {
+            ol: "a",
+            la: "a",
+            al: "a",
+            ad: "a",
+            do: "a",
+            ou: "a",
+        };
+
+        // oalaaalaaadaoau
+        const expectedElementsCount = {
+            o: 2,
+            d: 1,
+            a: 9,
+            l: 2,
+            u: 1,
+        };
+
+        const resultPolymer = getPolymerElementsCountAfterInsertions(
+            initialPolymer,
+            insertions,
+            1
+        );
+
+        assertEquals(resultPolymer, expectedElementsCount);
+    });
+
+    it("olaladou => 2 steps => o2d1a23l2u1", () => {
+        const initialPolymer = "olaladou";
+
+        const insertions = {
+            ol: "a",
+            la: "a",
+            al: "a",
+            ad: "a",
+            do: "a",
+            ou: "a",
+            oa: "a",
+            aa: "a",
+            da: "a",
+            ao: "a",
+            au: "a",
+        };
+
+        // oalaaalaaadaoau
+        const expectedElementsCount = {
+            o: 2,
+            d: 1,
+            a: 23,
+            l: 2,
+            u: 1,
+        };
+
+        const resultPolymer = getPolymerElementsCountAfterInsertions(
+            initialPolymer,
+            insertions,
+            2
+        );
+
+        assertEquals(resultPolymer, expectedElementsCount);
+    });
+});
--- src/days/day14/polymers.ts
+++ src/days/day14/polymers.ts
@@ -2,6 +2,8 @@ export type polymer = string;
 export type insertions = {
     [key: string]: string;
 };
+export type elementsCount = { [key: string]: number };
+type cache = { [key: string]: string };
 
 export function getPolymerAfterInsertions(
     initialPolymer: polymer,
@@ -59,8 +61,8 @@ export function getLeastCommonElementCount(polymer: polymer): number {
     return min;
 }
 
-function getElementsCount(polymer: polymer): { [key: string]: number } {
-    let elementsCount: { [key: string]: number } = {};
+export function getElementsCount(polymer: polymer): elementsCount {
+    let elementsCount: elementsCount = {};
 
     for (let i = 0; i < polymer.length; i++) {
         const currentChar = polymer[i];
@@ -73,4 +75,124 @@ function getElementsCount(polymer: polymer): { [key: string]: number } {
     }
 
     return elementsCount;
+}
+
+export function getOverlappingSubPolymers(
+    polymer: polymer,
+    length: number
+): polymer[] {
+    const polymers: polymer[] = [];
+
+    let i = 0;
+
+    while (i < polymer.length - 1) {
+        polymers.push(polymer.slice(i, i + length));
+        i += length - 1;
+    }
+
+    return polymers;
+}
+
+const INTERVAL = 10;
+
+export function getPolymerElementsCountAfterInsertions(
+    initialPolymer: polymer,
+    insertions: insertions,
+    steps: number
+): elementsCount {
+    const occurencesPerStepsCache: { [key: string]: elementsCount }[] =
+        Array.from(Array(steps + 1), () => ({}));
+    const cache: cache = {};
+
+    let occurencesPerStepsCacheUsageCount = 0;
+    let cacheUsageCount = 0;
+
+    const sumOccurences = getPolymersAfterInsertionsRec(initialPolymer, steps);
+
+    console.log("cache stats ♻️");
+    console.log(`\tcache was used ${cacheUsageCount} times! 🌸`);
+    console.log(
+        `\tcache per steps was used ${occurencesPerStepsCacheUsageCount} times! 🌸`
+    );
+
+    return sumOccurences;
+
+    function getPolymersAfterInsertionsRec(
+        polymer: polymer,
+        steps: number
+    ): elementsCount {
+        if (steps === 0) {
+            if (!occurencesPerStepsCache[steps][polymer]) {
+                occurencesPerStepsCache[steps][polymer] =
+                    getElementsCount(polymer);
+            } else {
+                occurencesPerStepsCacheUsageCount++;
+            }
+
+            return occurencesPerStepsCache[steps][polymer];
+        }
+
+        const subPolymers = getOverlappingSubPolymers(polymer, INTERVAL);
+        const polymersAfterInsert: polymer[] = [];
+
+        for (const subPolymer of subPolymers) {
+            if (!cache[subPolymer]) {
+                cache[subPolymer] = getPolymerAfterInsertions(
+                    subPolymer,
+                    insertions,
+                    1
+                );
+            } else {
+                cacheUsageCount++;
+            }
+
+            polymersAfterInsert.push(cache[subPolymer]);
+        }
+
+        const sumOccurences: elementsCount = {};
+
+        for (const polymer of polymersAfterInsert) {
+            if (!occurencesPerStepsCache[steps - 1][polymer]) {
+                occurencesPerStepsCache[steps - 1][polymer] =
+                    getPolymersAfterInsertionsRec(polymer, steps - 1);
+            } else {
+                occurencesPerStepsCacheUsageCount++;
+            }
+
+            const subElementsCount =
+                occurencesPerStepsCache[steps - 1][polymer];
+
+            addElementsCounts(sumOccurences, subElementsCount);
+        }
+
+        removeDoubleCounts(sumOccurences, polymersAfterInsert);
+
+        return sumOccurences;
+    }
+}
+
+function addElementsCounts(
+    elementsCountTarget: elementsCount,
+    elementCountSource: elementsCount
+) {
+    for (const [key, value] of Object.entries(elementCountSource)) {
+        if (!(key in elementsCountTarget)) {
+            elementsCountTarget[key] = 0;
+        }
+
+        elementsCountTarget[key] += value;
+    }
+}
+
+// Polymers are overlapping, thus we need to remove double counts
+function removeDoubleCounts(sumOccurences: elementsCount, polymers: polymer[]) {
+    for (const polymer of polymers) {
+        const characterFromNext = polymer[polymer.length - 1];
+
+        sumOccurences[characterFromNext] -= 1;
+    }
+
+    const lastPolymer = polymers[polymers.length - 1];
+    const theLastCharacter = lastPolymer[lastPolymer.length - 1];
+    sumOccurences[theLastCharacter] += 1;
 }
